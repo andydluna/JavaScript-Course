b93dfbbe3a586d2825e143ba34ed9e033e0d9eee
Andy De Luna <raidenshogun05@gmail.com>
2024-07-03 17:03:59 -0500

    Add a basic sticky behavior using js

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -80,8 +80,6 @@ tabsContainer.addEventListener('click', function (e) {
 
 // Menu fade animation
 const handleHover = function (e) {
-  console.log(this, e.currentTarget);
-
   if (e.target.classList.contains('nav__link')) {
     const link = e.target;
     const siblings = link.closest('.nav').querySelectorAll('.nav__link');
@@ -97,6 +95,17 @@ const handleHover = function (e) {
 nav.addEventListener('mouseover', handleHover.bind(0.5));
 
 nav.addEventListener('mouseout', handleHover.bind(1));
+
+// Sticky navigation
+window.addEventListener('scroll', function () {
+  const s1coords = section1.getBoundingClientRect();
+
+  if (s1coords.top <= 0) {
+    nav.classList.add('sticky');
+  } else {
+    nav.classList.remove('sticky');
+  }
+});
 
 /////////////////////////////////////
 /////////////////////////////////////
