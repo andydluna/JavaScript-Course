9572cc3f386f6d812efdba09e576f73a60d3c446
Andy De Luna <raidenshogun05@gmail.com>
2024-07-06 19:46:47 -0500

    Add chaining to methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -485,10 +485,13 @@ class Account {
 
   deposit(val) {
     this.#movements.push(val);
+    return this;
   }
 
   withdraw(val) {
     this.deposit(-val);
+
+    return this;
   }
 
   requestLoan(val) {
@@ -496,6 +499,7 @@ class Account {
       this.deposit(val);
       console.log(`Loan approved`);
     }
+    return this;
   }
 
   static helper() {
@@ -517,3 +521,7 @@ console.log(acc1.getMovements());
 console.log(acc1);
 
 Account.helper();
+
+// Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1);
