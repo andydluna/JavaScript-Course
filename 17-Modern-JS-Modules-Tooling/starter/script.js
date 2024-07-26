f5fea419d366e618b0c8360b8dd0474b27a69e09
Andy De Luna <raidenshogun05@gmail.com>
2024-07-26 13:43:34 -0500

    Introduction to Parcel

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -11,12 +11,12 @@
 
 // import add, { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 
-// import add, { cart } from './shoppingCart.js';
-// add('pizza', 2);
-// add('bread', 5);
-// add('apples', 4);
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
 
-// console.log(cart);
+console.log(cart);
 
 // console.log('Start fetching');
 // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -67,7 +67,8 @@
 // ShoppingCart2.addToCart('pizza', 2);
 // console.log(ShoppingCart2);
 
-import cloneDeep from '../node_modules/lodash-es/cloneDeep.js';
+// import cloneDeep from '../node_modules/lodash-es/cloneDeep.js';
+import cloneDeep from 'lodash-es';
 
 const state = {
   cart: [
@@ -82,3 +83,7 @@ const stateDeepClone = cloneDeep(state);
 state.user.loggedIn = false;
 console.log(stateClone);
 console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
